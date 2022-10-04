47f858c6f49dde906c0f79b3757caeefa288a50b
MousSozMois <joaquin_sena@hotmail.com>
2022-10-04 19:25:23 -0300

    Update PRACTICA.cs

--- PRACTICA.cs
+++ PRACTICA.cs
@@ -52,7 +52,7 @@ string nombreActual, nombreAnterior;
 for (int i=0; cantidadescuela; i++)
 {
     nombreActual=(string)escuela[i];
-    if !=(nombreActual == nombreAnterior) console.WriteLine(nombreActual);
+    if (nombreActual == nombreAnterior) console.WriteLine(nombreActual);
     nombreAnterior = nombreActual;
 }
 
