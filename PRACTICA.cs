d28e55eca356342ad7286c7d1a7e0eceb8fe09ec
MousSozMois <joaquin_sena@hotmail.com>
2022-10-04 19:07:50 -0300

    Update PRACTICA.cs

--- PRACTICA.cs
+++ PRACTICA.cs
@@ -42,7 +42,7 @@ console.WriteLine(listacompleta.count());
 //imprimir escuela por orden alfabetico
 foreach (ArrayList alumno in listacompleta)
 {
-    escuela.add(alumno[1])
+    escuela.add(alumno[1]);
 }
 console.WriteLine(escuela.count());
 //inciso E
@@ -52,7 +52,7 @@ string nombreActual, nombreAnterior;
 for (int i=0; cantidadescuela; i++)
 {
     nombreActual=(string)escuela[i];
-    if !(nombreActual == nombreAnterior) console.WriteLine(nombreActual);
+    if !=(nombreActual == nombreAnterior) console.WriteLine(nombreActual);
     nombreAnterior = nombreActual;
 }
 
